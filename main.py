bcae79d1e2691873280a26bc9e067a16f49a92f6
agusbrandone <tinbrandone@gmail.com>
2026-03-30 23:01:56 -0300

    Update main.py
    main menu

---
+++ main.py
@@ -0,0 +1,34 @@
+def mainMenu():
+    print("== FREELANCE HUB ==")
+    print("1. Projects")
+    print("2. Tasks")
+    print("3. Time Tracking")
+    print("4. Finances")
+    print("5. Exit")
+
+    choice = input("Choose an option: ")
+    return choice
+
+
+def apploop():
+    while True:
+        choice = mainMenu()
+        if choice == "1":
+            print("projects")
+        elif choice == "2":
+            print("Tasks")
+        elif choice == "3":
+            print("Time Tracking")
+        elif choice == "4":
+            print("Finances")
+        elif choice == "5":
+            break
+        else:
+            print("The chosen option is invalid")
+            
+if __name__ == "__main__":
+    apploop()
+
+    
+
+
