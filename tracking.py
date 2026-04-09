637e48264bec5e51cb552b9061d600540030e771
Franco Martino <95370336+FrancoMartino@users.noreply.github.com>
2026-04-09 16:17:22 -0300

    Funcion para arrancar sesion (basica para esperar a diccionarios)

---
+++ tracking.py
@@ -0,0 +1,11 @@
+import time
+
+def startSession(projectId):
+    """
+    Captura el datetime actual para marcar inicio de trabajo en un proyecto.
+
+    - input: projectId, Int
+
+    - output: Float
+    """
+    return time.time()
