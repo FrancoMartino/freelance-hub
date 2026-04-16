a2abf93b20e573bb63715eae78d188ad4b68ddc9
Franco Martino <95370336+FrancoMartino@users.noreply.github.com>
2026-04-16 16:33:26 -0300

    Formateo

--- tracking.py
+++ tracking.py
@@ -8,15 +8,47 @@ def startSession(projectId):
 
     - output: Float
     """
-    return time.time()
+    return time.time() # TODO: Logica de diccionarios
 
-def endSession(startTimestamp):
+def endSession(projectId, startTimestamp):
     """
     Captura datetime de fin, calcular diferencia con el inicio y retornar horas trabajadas.
 
+    - input: projectId, Float
     - input: startTimestamp, Float
 
     - output: Float
     """
+
+    # TODO: Falta toda la logica de guardado y diccionario
+
     calcHours = lambda start, end: round((end - start) / 3600, 4)
     return calcHours(startTimestamp, time.time())
+
+def accumulateHours(projectTotalHours, sessionNetHours):
+    """
+    Sumar las horas de la sesión actual al total histórico del proyecto en el diccionario.
+
+    - input: projectTotalHours, Float
+    - input: sessionNetHours, Float
+
+    - output: Float
+    """
+    
+    return projectTotalHours + sessionNetHours
+
+def generateTimeReport(allData):
+    """
+    Crear matriz resumen de horas por proyecto para mostrar en consola.
+
+    - input: allData, Dict
+
+    - output: List[List]
+    """
+    makeRow = lambda p_id, hrs: [p_id, hrs]
+    
+    reportMatrix = [["Project ID", "Total Hours"]]
+    for p_id, hrs in allData.items():
+        reportMatrix.append(makeRow(p_id, hrs))
+        
+    return reportMatrix
